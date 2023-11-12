270e630370ff547e813329c48a206d43e5fe3568
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-11-12 11:52:00 -0300

    Adicionar arquivos de projeto.

---
+++ C#-Requisicao-API/Modelos/Musicas.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Text.Json.Serialization;
+
+namespace C__Requisicao_API.Modelos;
+
+internal class Musicas
+{
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+
+    
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+
+    [JsonPropertyName("genre")]
+
+    public string? Genero{ get; set; }
+
+    public void ExibirDetalhesMusicas()
+    {
+        Console.WriteLine($" Nome : {Nome}");
+        Console.WriteLine($" Artista : {Artista}");
+        Console.WriteLine($" Duração em Segundos : {Duracao /1000}");
+        Console.WriteLine($" Genero:  {Genero}");
+    }
+}
---
+++ C#-Requisicao-API/Program.cs
@@ -0,0 +1,17 @@
+﻿using C__Requisicao_API.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musicas>> (resposta)!;
+        musicas[0].ExibirDetalhesMusicas();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Houve algum problema: {ex.Message}");
+    }
+}
+
